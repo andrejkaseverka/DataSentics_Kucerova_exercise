0f5e5020bd7e5dfb34f6ad33854419b031bfeb4b
Andrea Kučerová <andrejkaseverka@seznam.cz>
2023-02-12 14:54:20 +0100

    Učesáno řešení, které upravuje hledání pro titles

--- DataSentics_exercise.py
+++ DataSentics_exercise.py
@@ -30,27 +30,12 @@ for row in reader:
     dict_of_ISBN[row['Book-Title']].append(row['ISBN'])
 handle.close()
 
-## Searching for the ISBN of favourite book
-#favourite_ISBN = ''
-#handle = open(books_address, 'r')
-#reader = csv.DictReader(handle, delimiter=';')
-#for row in reader:
-#    if row['Book-Title']==str(favourite_book):
-#        favourite_ISBN = row['ISBN']
-#        break
-#handle.close()
-
 # Checking if the title is correct, if it is not, it exits the program
 if favourite_book not in dict_of_ISBN:
     print('Sorry, but I do not know this book. Are you sure you typed the title correctly?')
     sys.exit()
 
-#if favourite_ISBN=='':
-#    print('Sorry, but I do not know this book. Are you sure you typed the title correctly?')
-#    sys.exit()
-
 # Preparing list of 10 book suggestions, so far with empty strings
-#suggestions_ISBN = ['']*10
 suggestions_title = ['']*10
 
 # Getting through the table of ratings to find people that rated the favourite book 10 (or less if
@@ -111,15 +96,6 @@ while (suggestions_title[9]=='') and (rating_processing>5):
                     suggestions_title[i] = suggestions_title[i-1]
                 i -= 1
     rating_processing -= 1
-
-## Filling the names of the suggested books in suggestions_titles
-#handle = open(books_address, 'r')
-#reader = csv.DictReader(handle, delimiter=';')
-#for row in reader:
-#    for i in range(10):
-#        if row['ISBN']==suggestions_ISBN[i]:
-#            suggestions_titles[i] = row['Book-Title']
-#handle.close()
 
 # Printing the result
 print('\nI think you might like the following titles:\n')
